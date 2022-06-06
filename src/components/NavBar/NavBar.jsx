b5c7f9fb7fbef8739dacf2448d0fa914f32b5a51
Roger Davila <rogerddavila@gmail.com>
2022-06-06 15:36:51 -0700

    remove unused links and add notes link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,12 +12,8 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp; | &nbsp;
+      <Link to={`/${user.name}/notes`}>Notes</Link>
       <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;
       <Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
