d4217fa1634cc4a93881641247817c4e22baa96c
Roger Davila <rogerddavila@gmail.com>
2022-06-06 15:32:15 -0700

    add user field to schema

--- models/notebook.js
+++ models/notebook.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const notebookSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
   name: {
     type: String,
     minlength: 1,
     maxlength: 50
   }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 module.exports = mongoose.Model('Notebook', notebookSchema);
--- models/tag.js
+++ models/tag.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tagSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
   name: {
     type: String,
     minlength: 1,
