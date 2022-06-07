debaa7bbbf2cb7416be54ecd328abd4aeb182da6
Roger Davila <rogerddavila@gmail.com>
2022-06-07 16:51:08 -0700

    add routes to pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NotesPage from '../NotesPage/NotesPage';
+import NoteDetailPage from '../NoteDetailPage/NoteDetailPage';
 import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
@@ -13,11 +14,13 @@ function App() {
     <main className="App">
       { user ?
         <>
-          <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* Route components in here */}
-            <Route path={`/${user.name}/notes`} element={<NotesPage />} />
+            <Route path={`/${user.name}/notes`} element={<NotesPage user={user}/>} />
+            <Route path={`/${user.name}/notes/:noteId`} element={<NoteDetailPage user={user}/>} />
+            <Route path='/*' element={<Navigate to={`/${user.name}/notes`} />} />
           </Routes>
+          <NavBar user={user} setUser={setUser} />
         </>
         :
         <AuthPage setUser={setUser} />
