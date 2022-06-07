cf76e0af36f8e01dced6b2aed57defeebc2a4bdb
Roger Davila <rogerddavila@gmail.com>
2022-06-07 16:05:31 -0700

    add api request to create new note

--- src/utilities/notes-api.js
+++ src/utilities/notes-api.js
@@ -4,3 +4,7 @@ const BASE_URL = '/api/notes';
 export function getAll() {
   return sendRequest(`${BASE_URL}/notes/user`);
 }
+
+export function createNote() {
+  return sendRequest(`${BASE_URL}/notes/user`, 'POST');
+}
