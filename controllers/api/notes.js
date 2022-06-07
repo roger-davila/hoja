a9abfc0c5bef2a2be0f778ea34ecaab80eed51c2
Roger Davila <rogerddavila@gmail.com>
2022-06-07 15:48:41 -0700

    add controller function to create a new note

--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -1,10 +1,16 @@
 const Note = require('../../models/note');
 
 module.exports = {
-  getAllNotes
+  getAllNotes,
+  createNote
 }
 
 async function getAllNotes(req, res) {
-  const notes = await Note.find({ user: req.user._id }).sort('updatedAt');
+  const notes = await Note.find({ user: req.user._id }).sort('-updatedAt');
   return res.json(notes);
+}
+
+async function createNote(req, res) {
+  const note = await Note.create({ user: req.user._id});
+  return res.json(note);
 }
