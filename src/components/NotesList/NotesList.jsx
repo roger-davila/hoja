a2f9dca0e9871a3f47939ee77898a7fdd7799f48
Roger Davila <rogerddavila@gmail.com>
2022-06-07 15:53:20 -0700

    send user object to individual notes

--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -1,6 +1,6 @@
 import Note from "../Note/Note";
-export default function NotesList({ notes }) {
-  const notesList = notes.map((note) => <Note key={note._id} note={note} />);
+export default function NotesList({ notes, user }) {
+  const notesList = notes.map((note) => <Note key={note._id} note={note} user={user}/> );
   return (
     <section>
       {notesList}
