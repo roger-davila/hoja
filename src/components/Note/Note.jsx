6df63a7b0105d4d861e3a747044569ad1cfe803e
Roger Davila <rogerddavila@gmail.com>
2022-06-07 15:52:02 -0700

    add link component to notes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Note.css';
-export default function Note({ note }) {
+export default function Note({ note, user }) {
   return (
-    <article className="Note">
-      <h2>{note.title}</h2>
-      <p>{note.markdown_text}</p>
-      <p>{note.updatedAt}</p>
-      <p>{note.notebook}</p>
-    </article>
+    <Link className='note-link' to={`/${user.name}/notes/${note._id}`}>
+      <article className="Note">
+        <h3 className='note-title'>{note.title}</h3>
+        <p className='note-text-preview'>{note.markdown_text}</p>
+        <section className='note-details'>
+          <p className='last-updated'>{note.lastModified}</p>
+          <p className='notebook'>{note.notebook}</p>
+        </section>
+      </article>
+    </Link>
   )
 }
