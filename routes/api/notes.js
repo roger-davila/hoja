e909d511d24ab085f8bad68f60ce0a19020b9573
Roger Davila <rogerddavila@gmail.com>
2022-06-07 16:49:40 -0700

    add route for note detail page

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -4,5 +4,9 @@ const notesCtrl = require('../../controllers/api/notes');
 
 router.get('/notes/user', notesCtrl.getAllNotes);
 
+router.get('/notes/user/:noteId', notesCtrl.findNote);
+
 router.post('/notes/user', notesCtrl.createNote);
+
+
 module.exports = router;
