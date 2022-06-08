48db9c1faf557b84aeb31517d8101a2ea578861b
Roger Davila <rogerddavila@gmail.com>
2022-06-08 12:53:37 -0700

    add on change event listener for typing a note

--- src/pages/NoteDetailPage/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage/NoteDetailPage.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import * as notesAPI from '../../utilities/notes-api';
+import './NoteDetailPage.css';
+
 export default function NoteDetailPage() {
   const { noteId } = useParams();
   const [note, setNote] = useState({});
+
   useEffect(() => {
     async function getNote() {
       const note = await notesAPI.getNote(noteId);
@@ -11,12 +14,16 @@ export default function NoteDetailPage() {
     }
     getNote();
   }, []);
+
+  function handleTyping(evt) {
+    setNote({...note, [evt.target.name]: evt.target.value});
+  }
+  
   return (
-    <>
-      <h3>{note.title}</h3>
-      <p className=''>{note.lastModified}</p>
-      <textarea defaultValue={note.markdown_text}>
-      </textarea>
-    </>
+    <main className='NoteDetailPage'>
+      <h3 className='note-title'>{note.title}</h3>
+      <p className='note-last-modified'>{note.lastModified}</p>
+      <textarea className='note-field' value={note.markdown_text} name="markdown_text" onChange={handleTyping}></textarea>
+    </main>
   )
 }
