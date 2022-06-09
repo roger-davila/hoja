9596a486e28b828a9150381d860c0f9b8b7c3ffe
Roger Davila <rogerddavila@gmail.com>
2022-06-09 15:34:13 -0700

    include popupdrawer component and move button actions to the component

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import * as notesAPI from '../../utilities/notes-api';
 import NotesList from '../../components/NotesList/NotesList';
+import PopupDrawer from '../../components/PopupDrawer/PopupDrawer';
 import './NotesPage.css';
-export default function NotesPage({user}) {
+export default function NotesPage({ user }) {
   const [notes, setNotes] = useState([]);
-  let navigate = useNavigate();
-  async function handleNewNote () {
-    const newNote = await notesAPI.createNote();
-    navigate(`/${user.name}/notes/${newNote._id}`);
-  }
+
   useEffect(() => {
     const getNotes = async () => {
       const notes = await notesAPI.getAll();
@@ -17,11 +13,12 @@ export default function NotesPage({user}) {
     }
     getNotes();
   }, []);
+
   return (
     <main className=' NotesPage main-container'>
       <h1 className='header'>Notes</h1>
       <NotesList notes={notes} user={user} />
-      <button className='add-note-button' onClick={handleNewNote}>+</button>
+      <PopupDrawer page={'notes'} user={user} />
     </main>
   )
 } 
