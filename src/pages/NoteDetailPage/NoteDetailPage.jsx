c06fad6cf69407a9b939440e89ac5644c5a826f1
Roger Davila <rogerddavila@gmail.com>
2022-06-08 23:46:55 -0700

    add autosave functionality to note editor

--- src/pages/NoteDetailPage/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage/NoteDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import * as notesAPI from '../../utilities/notes-api';
 import './NoteDetailPage.css';
@@ -6,6 +6,7 @@ import './NoteDetailPage.css';
 export default function NoteDetailPage() {
   const { noteId } = useParams();
   const [note, setNote] = useState({});
+  const timer = useRef();
 
   useEffect(() => {
     async function getNote() {
@@ -15,15 +16,20 @@ export default function NoteDetailPage() {
     getNote();
   }, []);
 
+  useEffect(() => {
+    timer.current = setTimeout(() => notesAPI.saveNote(note), 1500);
+    return () => clearTimeout(timer.current);
+  }, [note]);
+
   function handleTyping(evt) {
     setNote({...note, [evt.target.name]: evt.target.value});
   }
   
   return (
-    <main className='NoteDetailPage'>
+    <main className='NoteDetailPage main-container'>
       <h3 className='note-title'>{note.title}</h3>
       <p className='note-last-modified'>{note.lastModified}</p>
-      <textarea className='note-field' value={note.markdown_text} name="markdown_text" onChange={handleTyping}></textarea>
+      <textarea className='note-field' value={note.markdown_text} name="markdown_text" onChange={handleTyping} placeholder='Note field...' ></textarea>
     </main>
   )
 }
