c88563cac7f96bf8146ba6d661209e1c8ef7ccef
Roger Davila <rogerddavila@gmail.com>
2022-06-09 15:35:00 -0700

    add delete request

--- src/utilities/notes-api.js
+++ src/utilities/notes-api.js
@@ -16,3 +16,7 @@ export function getNote(noteId) {
 export function saveNote(note) {
   return sendRequest(`${BASE_URL}/user/notes/note`, 'PUT', { note });
 }
+
+export function deleteNote(noteId) {
+  return sendRequest(`${BASE_URL}/user/notes/${noteId}`, 'DELETE');
+}
