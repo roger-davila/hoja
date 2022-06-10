2be1d2f9cdc57fbd9789fd1e0db5b2f59266f504
Roger Davila <rogerddavila@gmail.com>
2022-06-10 15:58:37 -0700

    add syntax highlighting and toggle markdown preview

--- src/pages/NoteDetailPage/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage/NoteDetailPage.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+import { marked } from 'marked';
 import * as notesAPI from '../../utilities/notes-api';
 import PopupDrawer from '../../components/PopupDrawer/PopupDrawer';
+import MarkdownPreview from '../../components/MarkdownPreview/MarkdownPreview';
 import './NoteDetailPage.css';
 
 export default function NoteDetailPage({ user }) {
   const { noteId } = useParams();
   const [note, setNote] = useState({});
+  const [isMarkdown, setIsMarkdown] = useState(false);
   const timer = useRef();
   const autoSave = useRef(false);
 
@@ -30,12 +33,35 @@ export default function NoteDetailPage({ user }) {
     setNote({ ...note, [evt.target.name]: evt.target.value });
   }
 
+  function parseNote() {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      highlight: function(code, lang) {
+        const hljs = require('highlight.js');
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+      },
+      langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+      pedantic: false,
+      gfm: true,
+      breaks: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false
+    });
+
+    const html = marked.parse(note.markdown_text);
+    return <MarkdownPreview html={html} />
+  }
+
   return (
     <main className='NoteDetailPage main-container'>
       <input className='note-title' name="title" onChange={handleTyping} value={note.title} />
       <p className='note-last-modified'>{note.lastModified}</p>
-      <textarea className='note-field' value={note.markdown_text} name="markdown_text" onChange={handleTyping} placeholder='Note field...' ></textarea>
-      <PopupDrawer page={'note'} user={user} note={note} />
+      {isMarkdown ? parseNote() : <textarea className='note-field' value={note.markdown_text} name="markdown_text" onChange={handleTyping} placeholder='Note field...' ></textarea>}
+
+      <PopupDrawer page={'note'} user={user} note={note} setIsMarkdown={setIsMarkdown} />
     </main>
   )
 }
