138a965e503055fc9556f7fd5f3d23187deb5f2c
Roger Davila <rogerddavila@gmail.com>
2022-07-06 16:28:30 -0700

    add ability to render demo page component on auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import DemoPage from '../DemoPage/DemoPage';
 import './AuthPage.css';
 
-export default function AuthPage({ setUser }) {
+export default function AuthPage({ setUser, demo }) {
+  function renderPage() {
+    if (demo) return <DemoPage />
+    else return <main className='main-container form-container'>{showSignUp ? <SignUpForm setUser={setUser} setShowSignUp={setShowSignUp} /> : <LoginForm setUser={setUser} setShowSignUp={setShowSignUp} />}</main>
+  }
   const [showSignUp, setShowSignUp] = useState(true);
   return (
-    <main className='main-container form-container'>
-      {showSignUp ?
-        <SignUpForm setUser={setUser} setShowSignUp={setShowSignUp} />
-        :
-        <LoginForm setUser={setUser} setShowSignUp={setShowSignUp} />
-      }
-    </main>
+    <>
+      {renderPage()}
+    </>
   );
 }
