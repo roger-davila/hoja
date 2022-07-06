d954e64333ce4c60fa428ec240b407e4407e25e2
Roger Davila <rogerddavila@gmail.com>
2022-07-06 16:02:54 -0700

    add demo page link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,9 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 import './NavBar.css';
 
-export default function NavBar({ user, setUser }) {
-
+export default function NavBar({ user, setUser, demo, setDemo }) {
   function handleLogOut() {
     // Remove token using the user service
     userService.logOut();
@@ -13,9 +13,17 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav className='NavBar'>
-      <Link className='menu-link' to={`/${user.name}/notes`}>Notes</Link>
-      <Link className='menu-link' to={`/${user.name}/notebooks`}>Notebooks</Link>
-      <Link className='menu-link' to="" onClick={handleLogOut}>Log Out</Link>
+      {user ? (
+        <>
+          <Link className='menu-link' to={`/${user.name}/notes`}>Notes</Link>
+          <Link className='menu-link' to={`/${user.name}/notebooks`}>Notebooks</Link>
+          <Link className='menu-link' to="" onClick={handleLogOut}>Log Out</Link>
+        </>
+      ) : (
+        <>
+          <Link className='menu-link' to="" onClick={()=> setDemo(!demo)}>{demo ? 'Demo Page' : 'Back to Auth Page'}</Link>
+        </>
+      )}
     </nav>
   );
 }
